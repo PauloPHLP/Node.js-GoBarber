e36c3cb4500006ba9ed4f8544b6dd8e0f06d552f
Paulo <paulo.phlp@outlook.com>
2020-09-03 11:12:48 -0300

    Feature: Add Amazon S3 file upload.

---
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+import HandlerbarsMailTemplateProvider from './implementations/HandlerbarsMailTemplateProvider';
+
+const providers = {
+  handlebars: HandlerbarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);
