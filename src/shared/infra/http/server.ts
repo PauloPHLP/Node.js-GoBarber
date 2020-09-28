ad80c4f2f8e3859ca3ec1383d8f53903141cd4f5
Paulo <paulo.phlp@outlook.com>
2020-09-28 10:40:05 -0300

    Chore: Change rate limiter for files.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -14,12 +14,12 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(errors);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
